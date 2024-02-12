ce7c6910365ef2bb85916f3da73a42a0e008fe53
Rhoeldain Pascua <deyndeyn727@gmail.com>
2024-02-12 17:49:09 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
